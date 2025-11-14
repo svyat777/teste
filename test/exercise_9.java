1ff1c3aa455f5eb71205213c92a86e9a665566c8
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 16:05:50 +0000

    exercise_9.java

--- test/exercise_9.java
+++ test/exercise_9.java
@@ -2,20 +2,21 @@ package sass;
 public class exercise_9 {
     public static void main(String[] args) {
 
-        int[] nums = new int[51];
+        int[] nums = new int[51];//creating array with 51 vals 
         int rest = 0;
 
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1; i <= 50; i++) {//cycle to fill the array
             nums[i - 1] = i;
         }
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {//cycle to verify odd nums
             rest = nums[i]% 2;
            if(rest == 1){
-            System.out.println(nums[i]);
+            System.out.println(nums[i]);//print odd nums from array 
            }
          } 
          
     }
 
 }
+
