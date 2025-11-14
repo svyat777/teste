7d57842b47bc9c816460070bebf9b85278b479f8
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 15:20:54 +0000

    exrcice_1.java

--- test/exrcice_1.java
+++ test/exrcice_1.java
@@ -6,19 +6,19 @@ public class exrcice_1 {
         Scanner input = new Scanner(System.in);
         boolean user_answer = false;
 
-        while (user_answer == false) {
-            System.out.print("enter number from 0 to 10: ");
+        while (user_answer == false) {//cycle if user writes wrong number
+            System.out.print("enter number from 0 to 10: ");//ask user for a number 
             int user_number = input.nextInt(); 
             
             if(user_number < 0 || user_number > 10){
                 System.out.println("please enter correct number! from 0 to 10: "); 
             }else{
                 System.out.println(user_number);
-                user_answer = true;
+                user_answer = true; //makes filter end code coze user writes right number
             }
        }
 
-    }
-
+    } 
 
 }
+
