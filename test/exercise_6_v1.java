5455c1316713864f8438e5b1dc2332ecdda0022f
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 15:53:08 +0000

    exercise_6_v1.java

--- test/exercise_6_v1.java
+++ test/exercise_6_v1.java
@@ -2,10 +2,11 @@ package sass;
 public class exercise_6_v1 {
     public static void main(String[] args) {
         
-        for(int i = 0; i < 20; i++) {
+        for(int i = 0; i < 20; i++) {//prints numbers from 1 to 20 below 
             System.out.println(i);
          }
     }
 }
+
 
 
