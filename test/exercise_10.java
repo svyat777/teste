829ee1c601eddda70ae51177f60071e1bbc958c9
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 16:07:49 +0000

    exercise_10.java

--- test/exercise_10.java
+++ test/exercise_10.java
@@ -2,11 +2,11 @@ package sass;
 public class exercise_10 {
     public static void main(String[] args) {
         
-        int number_1 = 4;
-        int number_2 = 15; 
-        int sum = 0;
+        int number_1 = 4;//num 1
+        int number_2 = 15; //num 2
+        int sum = 0;// sum
 
-        for(int i = 0; i <= number_2; i++) {
+        for(int i = 0; i <= number_2; i++) {//cycle 4 show integert between 2 nums and sum of them
             sum += number_1;
             i = number_1;
             System.out.print(i + " ");
@@ -15,5 +15,6 @@ public class exercise_10 {
          System.out.println("summ is: " + sum);
     }
 }
+
 
 
