56df985fc0016c992ba197c36193a1891d29b315
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 15:59:44 +0000

    exercise_8.java

--- test/exercise_8.java
+++ test/exercise_8.java
@@ -2,16 +2,17 @@ package sass;
 public class exercise_8 {
     public static void main(String[] args) {
 
-        int [] nums = {1,7,3,9,5};
-        int average = 0;
-        int sum = 0;
+        int [] nums = {1,7,3,9,5};//array
+        int average = 0;//val 4 avearge 
+        int sum = 0;//val 4 sum of nums
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {//cycle 4 sum of nums
            sum = sum + nums[i];
          } 
-         average = sum / nums.length;
-         System.out.println("there is the summ of numbers: " + sum);
-         System.out.println("there is the average of numbers: " + average);
+         average = sum / nums.length;//average num calcs
+         System.out.println("there is the summ of numbers: " + sum);//print sum
+         System.out.println("there is the average of numbers: " + average);//print average
     }
 
 }
+
