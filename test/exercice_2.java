e46dd027bae1fa3449a583e0eab20a30c6108275
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 15:25:48 +0000

    exercice_2.java

--- test/exercice_2.java
+++ test/exercice_2.java
@@ -6,20 +6,20 @@ public class exercice_2 {
         Scanner input = new Scanner(System.in);
         boolean user_answer = false;
 
-        while (user_answer == false) {
-            System.out.print("enter your username: ");
+        while (user_answer == false) {//cycle for right password and username 
+            System.out.print("enter your username: ");//ask user for username
             String  user_name = input.nextLine(); 
             
-            System.out.print("enter your password: ");
+            System.out.print("enter your password: ");//ask user for pass
             String  user_pass = input.nextLine(); 
 
-            if(!user_name.equals(user_pass)){
+            if(!user_name.equals(user_pass)){//compares username and pass if they r the same
                 System.out.println("your name is: " + user_name);
                 System.out.println("your password is: " + user_pass);
                 user_answer = true;
                 
             }else{
-                System.out.println("password and username cant be the same please write another one: ");
+                System.out.println("password and username cant be the same please write another one: ");//error
             }
        }
 
@@ -27,3 +27,4 @@ public class exercice_2 {
 
 
 }
+
