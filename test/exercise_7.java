be984b4adb26256d36922582741f3ce7aaf7801c
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-11-14 15:56:31 +0000

    exercise_7.java

--- test/exercise_7.java
+++ test/exercise_7.java
@@ -2,15 +2,16 @@ package sass;
 public class exercise_7 {
     public static void main(String[] args) {
 
-        int [] nums = {1,7,3,9,5};
-        int biggest_num = 0;
+        int [] nums = {1,7,3,9,5};//array
+        int biggest_num = 0;//val 4 biggest num
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {//founds biggest num
            if(nums[i] > biggest_num){
             biggest_num = nums[i];
            }
          } 
-         System.out.println(biggest_num);
+         System.out.println(biggest_num);//print biggest num
     }
 
 }
+
